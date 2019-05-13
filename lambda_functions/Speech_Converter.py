92b13a1efaaaf37291dbce3f215817b625620d28
CP <chandra.prakash363@gmail.com>
2019-05-13 10:23:49 +0530

    Update Speech_Converter.py

--- lambda_functions/Speech_Converter.py
+++ lambda_functions/Speech_Converter.py
@@ -3,10 +3,12 @@ import boto3
 
 print("calling lambda fucntion")
 
+
+polly = boto3.client('polly')
 bucket = 'serverless-polly.com'
 
 
-def lambda_handler(event, context):
+def lambda_handler(event,context):
     print("event", event)
     
     requested_resource = event['resource']
@@ -16,22 +18,39 @@ def lambda_handler(event, context):
     else:
       return get_call_method(requested_resource,event,bucket)
 
-def post_call_method(requested_resource,table,event):
-    if '/lambda_functions/login' == requested_resource:
-        return login_call(table, event)
+def post_call_method(requested_resource,event,bucket):
+    if '/lambda_functions/TextToSpeech' == requested_resource:
+        return TextToSpeech(event,bucket)
     else:
-         return create_response(body='Not Found.', status_code=500)
+         return create_response(body='URL Not Found.', status_code=500)
         
-def get_call_method(requested_resource,table,event,bucket):
-    if '/lambda_functions/upload' == requested_resource:
-        return sign_s3(event,bucket)
-    else:
-        return create_response(body='Not Found', status_code=500)
+def get_call_method(requested_resource,event,bucket):
+        return create_response(body='URL Not Found', status_code=500)
 
 
+def TextToSpeech(event,bucket):
+    if 'body' in event and event['body'] is not None:
+        body = event['body']
 
+    print(body)
 
+    response = polly.start_speech_synthesis_task(
+        OutputFormat= 'mp3',
+        OutputS3BucketName= bucket,
+        OutputS3KeyPrefix='myrecording-',
+        SampleRate= '22050',
+        Text= body,
+        TextType= 'text',
+        VoiceId='Aditi',
+        LanguageCode='en-US'
+    )
+    
+    gen_url= response['SynthesisTask']['OutputUri']
 
+    print(response)    
+
+    return create_response(gen_url)
+        
 
 def create_response(body, status_code=200):
     body_json = {'message': body, 'status_code':status_code}
